c05126a6542e73c29eab13bb92ebb3fb89ace9b3
dwanGim <nugoory20@gmail.com>
2023-08-03 17:15:46 +0900

    baekjoon0803

--- archive.py
+++ archive.py
@@ -115,3 +115,43 @@ from collections import Counter
 #     prize = max(dice) * 100  
 
 # print(prize)
+
+# #2739
+# gugu = int(input())
+
+# for i in range(1, 10):
+#     print(f"{gugu} * {i} = {gugu*i}")
+
+# # 10950
+# ran = int(input())
+# for _ in range(ran):
+#     A,B = map(int, input().split())
+#     print(A+B)
+
+# #8393
+# n = int(input())
+# total = 0
+# for i in range(1, n+1):
+#     total += i
+
+# print(total)
+
+# # 25304
+# X = int(input())
+# N = int(input())
+# total = 0
+# YN = "No"
+# for _ in range(N) :
+#     a,b = map(int, input().split())
+#     total += a*b
+
+# if total == X :
+#     YN = "Yes"
+
+# print(YN)
+
+# # 25314
+# N = int(input())
+# longlong = N//4
+# str = "long " * longlong
+# print(str+"int")
