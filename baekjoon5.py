8f5b4bbfc2496fb097afe94f78bb3b514be5128b
dwanGim <nugoory20@gmail.com>
2023-08-23 17:53:11 +0900

    230823

---
+++ baekjoon5.py
@@ -0,0 +1,31 @@
+import sys
+
+# # 2588
+# num1 = int(input())
+# if num1 <= 99 or num1 >= 1000:
+#     print("세 자리 자연수를 입력해주세요.")
+#     exit()
+# num2 = int(input())
+# if num2 <= 99 or num2 >= 1000:
+#     print("세 자리 자연수를 입력해주세요.")
+#     exit()
+
+# result1 = num1 * (num2 % 10)
+# result2 = num1 * ((num2 // 10) % 10)
+# result3 = num1 * (num2 // 100)
+# result4 = num1 * num2
+
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
+
+# # 11382
+# a, b, c = map(int, input().split())
+# print(a+b+c)
+
+print("|\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
