33301595b6935d4c2be2b52619d904633b6377ea
dwanGim <nugoory20@gmail.com>
2023-08-25 18:26:40 +0900

    230825

--- baekjoon5.py
+++ baekjoon5.py
@@ -42,12 +42,35 @@ import sys
 
 # print(*baskets)
 
-# 10813
-N, M = map(int, input().split())
-balls = list(range(1, N+1))
+# # 10813
+# N, M = map(int, input().split())
+# balls = list(range(1, N+1))
 
-for _ in range(M):
-    i, j = map(int, input().split())
-    balls[i-1], balls[j-1] = balls[j-1], balls[i-1]
+# for _ in range(M):
+#     i, j = map(int, input().split())
+#     balls[i-1], balls[j-1] = balls[j-1], balls[i-1]
 
-print(*balls)
+# print(*balls)
+
+# # 5597
+# students = set()
+# for _ in range(28):
+#     n = int(input())
+#     students.add(n)
+
+# nums = set(range(1,31))
+# result = nums.difference(students)
+# print(min(result))
+# print(max(result))
+
+# 3052
+# set은 중복되지 않은 값들의 집합을 나타내며
+# 배열의 원소들을 set으로 변환하여 중복되지 않은 값들만 남습니다.
+# 이후 set의 길이를 확인하면 서로 다른 값의 개수를 얻을 수 있습니다.
+n = []
+for _ in range(10):
+    x = int(input())
+    n.append(x%42)
+
+cnt = len(set(n))
+print(cnt)
