23dfa5b696936fb9e8661d3f0317b476b071e6d9
dwanGim <nugoory20@gmail.com>
2023-11-14 21:44:56 +0900

    231114

--- baekjoon7.py
+++ baekjoon7.py
@@ -2,16 +2,16 @@
 
 # print((2**int(input())+1)**2)
 
-# 5086
-while True:
-    a, b = map(int, input().split())
+# # 5086
+# while True:
+#     a, b = map(int, input().split())
 
-    if a == 0 and b == 0:
-        break
+#     if a == 0 and b == 0:
+#         break
 
-    if b % a == 0:
-        print("factor")
-    elif a % b == 0:
-        print("multiple")
-    else:
-        print("neither")
+#     if b % a == 0:
+#         print("factor")
+#     elif a % b == 0:
+#         print("multiple")
+#     else:
+#         print("neither")
