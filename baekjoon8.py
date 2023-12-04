77e3b100747f70dc090763e7de8ffe4bad969e4b
dwanGim <nugoory20@gmail.com>
2023-12-04 19:31:47 +0900

    231204

---
+++ baekjoon8.py
@@ -0,0 +1,50 @@
+# #2739
+# gugu = int(input())
+
+# for i in range(1, 10):
+#     print(f"{gugu} * {i} = {gugu*i}")
+
+# # 10950
+# ran = int(input())
+# for _ in range(ran):
+#     A,B = map(int, input().split())
+#     print(A+B)
+
+# #8393
+# n = int(input())
+# total = 0
+# for i in range(1, n+1):
+#     total += i
+
+# print(total)
+
+# # 25304
+# X = int(input())
+# N = int(input())
+# total = 0
+# YN = "No"
+# for _ in range(N) :
+#     a,b = map(int, input().split())
+#     total += a*b
+
+# if total == X :
+#     YN = "Yes"
+
+# print(YN)
+
+# # 25314
+# N = int(input())
+# longlong = N//4
+# str = "long " * longlong
+# print(str+"int")
+
+# # 10871
+# N, X = map(int, input().split())
+# A = list(map(int, input().split()))
+
+# result = []
+# for num in A:
+#     if num < X:
+#         result.append(num)
+
+# print(*result)
